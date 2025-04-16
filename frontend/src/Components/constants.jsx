478996cc9099f9fc039f8913699e83797cb26030
Eraj Tanweer <erajtanweer2@gmail.com>
2025-04-16 14:26:36 +0500

    Deleted entire codebase and removed folders

--- frontend/src/Components/constants.jsx
+++
@@ -1,5 +0,0 @@
-// export const backendUrl = "https://backend-snowy-delta.vercel.app";
-// require('dotenv').config();
-// export const backendUrl = process.env.BACKEND_URL;
-export const backendUrl = "https://deliberatives.com/api";
-// export const backendUrl = "http://localhost:2000"
